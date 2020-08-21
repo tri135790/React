6c05ef74c16fa46675295d82fc141e9daef0b0b3
tri135790 <tranthanhtrivn@gmail.com>
2020-08-21 22:24:46 +0200

    add About Us page Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import DishDetail from './DishdetailComponent';
 import Header from'./HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import {DISHES} from '../shared/dishes';
 import {COMMENTS} from '../shared/comments';
 import {LEADERS} from '../shared/leaders';
@@ -48,7 +49,8 @@ class Main extends Component {
         <Switch>
             <Route path="/home" component={HomePage} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-            <Route path="/menu/:dishId" component={DishWithId}/>  
+            <Route path="/menu/:dishId" component={DishWithId}/> 
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} /> }/>
             <Route exact path="/contactus" component={Contact} />
             <Redirect to="/home" />
         </Switch>
