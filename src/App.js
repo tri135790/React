fe89fb1714cb2f6097b1f63b64b377cb383c8d39
tri135790 <tranthanhtrivn@gmail.com>
2020-08-23 12:38:02 +0200

    add Redux

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 import { BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 // import { render } from '@testing-library/react';
+
+const store = ConfigureStore();
 
 class App extends Component {
   
-
+  
   render() {
     return (
-      <BrowserRouter>
-        <div >
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div >
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
