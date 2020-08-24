e93150a149932f5fcfcf7343919f283d97208957
tri135790 <tranthanhtrivn@gmail.com>
2020-08-24 14:16:27 +0200

    integrate Redux Thunk and Redux Logger to creat Loading page

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,13 +3,29 @@ import {Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem
          Modal,ModalBody,ModalHeader,Label,Row} from 'reactstrap';
 import {Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
-
+import {Loading} from './LoadingComponent';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
 
     const DishDetail = (props) => {
-       if (props.dish != null) {
+        if(props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        } else if ( props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            )
+        } else if (props.dish != null) {
            return (
                <div className="container">
                    <div className="row">
